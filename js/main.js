39f4abfcaaf89717394b724891f3b170317fc54d
Jimmy Dong <jndong88@gmail.com>
2016-12-02 16:42:45 -0800

    Added markup, styling and a little js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-console.log('js loaded!');
+/*--- variables ---*/
+
+var input, firstNum, op, result;
+var displayEl = document.getElementById('display');
+
+/*--- event listeners ---*/
+
+
+
+
+
+/*--- functions ---*/
+
+fuction initialize() {
+  input = '';
+  firstNum = op = result = null;
+  updateDisplay();
+}
+
+
+function updateDisplay() {
+  var text = result || input || '0.';
+  displayEl.textContent = text;
+}
+
+initialize();
+
+
